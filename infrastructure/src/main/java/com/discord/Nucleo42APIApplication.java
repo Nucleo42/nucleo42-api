8bfa3bb6cb75913c8abc55d08889a0a62cebd82b
Mikhael D'Amato <damato578@gmail.com>
2025-01-13 21:24:42 -0300

    chore: add spring boot application

---
+++ infrastructure/src/main/java/com/discord/Nucleo42APIApplication.java
@@ -0,0 +1,11 @@
+package com.discord;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Nucleo42APIApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(Nucleo42APIApplication.class, args);
+    }
+}
