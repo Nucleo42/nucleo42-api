b23a10b9eef8d0e8696039f9c590804edf199de2
matheusgondra <matheusgondra951@gmail.com>
2025-01-18 23:24:09 -0300

    feat: create role model

---
+++ core/src/main/java/com/discord/entity/Role.java
@@ -0,0 +1,12 @@
+package com.discord.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Role {
+    private String name;
+}
