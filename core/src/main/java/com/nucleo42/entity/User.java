3e8c1f73334ba1396afeebcadf51e670547b8766
Mikhael D'Amato <damato578@gmail.com>
2025-01-27 11:57:01 -0300

    bugfix: remove lombok lib from user entity

--- core/src/main/java/com/nucleo42/entity/User.java
+++ core/src/main/java/com/nucleo42/entity/User.java
@@ -1,14 +1,8 @@
 package com.nucleo42.entity;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
 import java.util.List;
+import java.util.Objects;
 
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
 public class User {
     private String firstName;
     private String lastName;
@@ -16,4 +10,88 @@ public class User {
     private String password;
     private String biography;
     private List<Skill> skills;
+
+    public User(String firstName, String lastName, String email, String password, String biography, List<Skill> skills) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.password = password;
+        this.biography = biography;
+        this.skills = skills;
+    }
+
+    public User() {
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getBiography() {
+        return biography;
+    }
+
+    public void setBiography(String biography) {
+        this.biography = biography;
+    }
+
+    public List<Skill> getSkills() {
+        return skills;
+    }
+
+    public void setSkills(List<Skill> skills) {
+        this.skills = skills;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return Objects.equals(firstName, user.firstName) && Objects.equals(lastName, user.lastName) && Objects.equals(email, user.email) && Objects.equals(password, user.password) && Objects.equals(biography, user.biography) && Objects.equals(skills, user.skills);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(firstName, lastName, email, password, biography, skills);
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", email='" + email + '\'' +
+                ", password='" + password + '\'' +
+                ", biography='" + biography + '\'' +
+                ", skills=" + skills +
+                '}';
+    }
 }
