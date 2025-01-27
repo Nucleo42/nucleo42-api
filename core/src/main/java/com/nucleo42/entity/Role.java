028d5c3c7d33eb3ae13c13ae1e39bd17bd2ff6c6
Mikhael D'Amato <damato578@gmail.com>
2025-01-27 11:58:47 -0300

    bugfix: remove lombok lib from role entity

--- core/src/main/java/com/nucleo42/entity/Role.java
+++ core/src/main/java/com/nucleo42/entity/Role.java
@@ -1,12 +1,41 @@
 package com.nucleo42.entity;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import java.util.Objects;
 
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
 public class Role {
     private String name;
+
+    public Role(String name) {
+        this.name = name;
+    }
+
+    public Role() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Role role = (Role) o;
+        return Objects.equals(name, role.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(name);
+    }
+
+    @Override
+    public String toString() {
+        return "Role{" +
+                "name='" + name + '\'' +
+                '}';
+    }
 }
