28455c03e4c94671a3830cc4df3e205316498b6e
Mikhael D'Amato <damato578@gmail.com>
2025-01-27 11:49:05 -0300

    bugfix: remove lombok lib from technology entity

--- core/src/main/java/com/nucleo42/entity/Technology.java
+++ core/src/main/java/com/nucleo42/entity/Technology.java
@@ -1,12 +1,41 @@
 package com.nucleo42.entity;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import java.util.Objects;
 
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
 public class Technology {
     private String name;
+
+    public Technology() {
+    }
+
+    public Technology(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Technology that = (Technology) o;
+        return Objects.equals(name, that.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(name);
+    }
+
+    @Override
+    public String toString() {
+        return "Technology{" +
+                "name='" + name + '\'' +
+                '}';
+    }
 }
