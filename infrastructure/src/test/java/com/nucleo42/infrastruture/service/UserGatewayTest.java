65ef2389b93d5ff0f1a74f9e144b813db49c18f7
matheusgondra <matheusgondra951@gmail.com>
2025-01-31 10:35:46 -0300

    test: ensure UserGateway call UserRepository.save with correct value

---
+++ infrastructure/src/test/java/com/nucleo42/infrastruture/service/UserGatewayTest.java
@@ -0,0 +1,42 @@
+package com.nucleo42.infrastruture.service;
+
+import com.nucleo42.entity.User;
+import com.nucleo42.infrastruture.entity.UserEntity;
+import com.nucleo42.infrastruture.mapper.UserMapper;
+import com.nucleo42.infrastruture.repository.UserRepository;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.mockito.Mockito.mockStatic;
+import static org.mockito.Mockito.verify;
+
+@ExtendWith(MockitoExtension.class)
+class UserGatewayTest {
+    @InjectMocks
+    private UserGateway sut;
+
+    @Mock
+    private UserRepository repository;
+
+    @Mock
+    private UserEntity userEntity;
+
+    @Mock
+    private User user;
+
+    @Test
+    @DisplayName("Should call UserRepository.save with correct value")
+    void test01() {
+        try (var mockedUserMapper = mockStatic(UserMapper.class)) {
+            mockedUserMapper.when(() -> UserMapper.toEntity(user)).thenReturn(userEntity);
+
+            sut.add(user);
+
+            verify(repository).save(userEntity);
+        }
+    }
+}
