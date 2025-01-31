ba05f2381e5c179aaaa725ee0ee215be7151fd47
matheusgondra <matheusgondra951@gmail.com>
2025-01-30 23:06:14 -0300

    test: ensure AddAccountImpl throw UserAlreadyExistsException when AddAccountGateway returns false

--- application/src/test/java/com/nucleo42/application/usecase/AddAccountImplTest.java
+++ application/src/test/java/com/nucleo42/application/usecase/AddAccountImplTest.java
@@ -2,6 +2,7 @@ package com.nucleo42.application.usecase;
 
 import com.nucleo42.application.gateway.AddAccountGateway;
 import com.nucleo42.entity.User;
+import com.nucleo42.exception.UserAlreadyExistsException;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -9,7 +10,9 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 class AddAccountImplTest {
@@ -26,5 +29,14 @@ class AddAccountImplTest {
         sut.add(user);
 
         verify(addAccountGateway).add(user);
+    }
+
+    @Test
+    @DisplayName("Should throw UserAlreadyExistsException when AddAccountGateway returns false")
+    void test02() {
+        var user = new User("John", "Doe", "johndoe@mail.com", "Password@123", null, null);
+        when(addAccountGateway.add(user)).thenReturn(false);
+
+        assertThrows(UserAlreadyExistsException.class, () -> sut.add(user));
     }
 }
