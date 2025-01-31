27a575d8bfb46737b6791a7e6ad152ebe0d8ec0c
matheusgondra <matheusgondra951@gmail.com>
2025-01-30 23:25:05 -0300

    test: ensure AddAccountImpl call Hasher with correct value

--- application/src/test/java/com/nucleo42/application/usecase/AddAccountImplTest.java
+++ application/src/test/java/com/nucleo42/application/usecase/AddAccountImplTest.java
@@ -1,8 +1,10 @@
 package com.nucleo42.application.usecase;
 
 import com.nucleo42.application.gateway.AddAccountGateway;
+import com.nucleo42.application.protocol.Hasher;
 import com.nucleo42.entity.User;
 import com.nucleo42.exception.UserAlreadyExistsException;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -11,8 +13,7 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 class AddAccountImplTest {
@@ -22,21 +23,37 @@ class AddAccountImplTest {
     @Mock
     private AddAccountGateway addAccountGateway;
 
+    @Mock
+    private Hasher hasher;
+
+    private final User testUser = new User("John", "Doe", "johndoe@mail.com", "Password@123", null, null);
+
+    @BeforeEach
+    void setup() {
+        when(addAccountGateway.add(testUser)).thenReturn(true);
+    }
+
     @Test
     @DisplayName("Should call AddAccountGateway with correct value")
     void test01() {
-        var user = new User("John", "Doe", "johndoe@mail.com", "Password@123", null, null);
-        sut.add(user);
+        sut.add(testUser);
 
-        verify(addAccountGateway).add(user);
+        verify(addAccountGateway).add(testUser);
     }
 
     @Test
     @DisplayName("Should throw UserAlreadyExistsException when AddAccountGateway returns false")
     void test02() {
-        var user = new User("John", "Doe", "johndoe@mail.com", "Password@123", null, null);
-        when(addAccountGateway.add(user)).thenReturn(false);
+        when(addAccountGateway.add(testUser)).thenReturn(false);
 
-        assertThrows(UserAlreadyExistsException.class, () -> sut.add(user));
+        assertThrows(UserAlreadyExistsException.class, () -> sut.add(testUser));
+    }
+
+    @Test
+    @DisplayName("Should call Hasher with correct value")
+    void test03() {
+        sut.add(testUser);
+
+        verify(hasher).hash("Password@123");
     }
 }
