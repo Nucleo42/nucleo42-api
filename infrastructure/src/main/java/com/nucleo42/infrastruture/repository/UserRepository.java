a40d242a51671735da0105b039e0d2af0f68466e
matheusgondra <matheusgondra951@gmail.com>
2025-01-31 10:35:41 -0300

    feat: ensure UserGateway call UserRepository.save with correct value

---
+++ infrastructure/src/main/java/com/nucleo42/infrastruture/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.nucleo42.infrastruture.repository;
+
+import com.nucleo42.infrastruture.entity.UserEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface UserRepository extends JpaRepository<UserEntity, UUID> {
+}
