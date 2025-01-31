e113c98d9d43b1b59b4ec5c44ac9f3181a4c01c5
matheusgondra <matheusgondra951@gmail.com>
2025-01-30 23:24:46 -0300

    feat: ensure AddAccountImpl call Hasher with correct value

--- application/src/main/java/com/nucleo42/application/usecase/AddAccountImpl.java
+++ application/src/main/java/com/nucleo42/application/usecase/AddAccountImpl.java
@@ -1,19 +1,24 @@
 package com.nucleo42.application.usecase;
 
 import com.nucleo42.application.gateway.AddAccountGateway;
+import com.nucleo42.application.protocol.Hasher;
 import com.nucleo42.entity.User;
 import com.nucleo42.exception.UserAlreadyExistsException;
 import com.nucleo42.usecase.AddAccount;
 
 public class AddAccountImpl implements AddAccount {
     private final AddAccountGateway addAccountGateway;
+    private final Hasher hasher;
 
-    public AddAccountImpl(AddAccountGateway addAccountGateway) {
+    public AddAccountImpl(AddAccountGateway addAccountGateway, Hasher hasher) {
         this.addAccountGateway = addAccountGateway;
+        this.hasher = hasher;
     }
 
     @Override
     public String add(User user) {
+        this.hasher.hash(user.getPassword());
+
         var result = this.addAccountGateway.add(user);
         if (!result) {
             throw new UserAlreadyExistsException();
