90cd725ebe5b42aad3cd7054a4df336bf01f5a74
matheusgondra <matheusgondra951@gmail.com>
2025-02-02 13:09:34 -0300

    doc: doc signup route

--- infrastructure/src/main/java/com/nucleo42/infrastruture/controller/SignUpController.java
+++ infrastructure/src/main/java/com/nucleo42/infrastruture/controller/SignUpController.java
@@ -3,8 +3,14 @@ package com.nucleo42.infrastruture.controller;
 import com.nucleo42.entity.User;
 import com.nucleo42.exception.AcceptTermsException;
 import com.nucleo42.exception.UserAlreadyExistsException;
+import com.nucleo42.infrastruture.annotation.ApiRequestBody;
 import com.nucleo42.infrastruture.dto.SignUpRequestDTO;
 import com.nucleo42.usecase.AddAccount;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.ExampleObject;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -19,6 +25,52 @@ public class SignUpController {
     @Autowired
     private AddAccount usecase;
 
+    @Operation(
+            description = "Create a new account",
+            summary = "Sign up"
+    )
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "201",
+                    description = "Account created",
+                    content = @Content(
+                            mediaType = "text/plain",
+                            examples = @ExampleObject(value = "User registered successfully")
+                    )
+            ),
+            @ApiResponse(
+                    responseCode = "400",
+                    description = "Bad request",
+                    content = @Content(
+                            mediaType = "text/plain",
+                            examples = @ExampleObject(value = "User already exists")
+                    )
+            ),
+            @ApiResponse(
+                    responseCode = "500",
+                    description = "Internal server error",
+                    content = @Content(
+                            mediaType = "text/plain",
+                            examples = @ExampleObject(value = "Internal server error")
+                    )
+            )
+    })
+    @ApiRequestBody(
+            content = @Content(
+                    mediaType = "application/json",
+                    examples = @ExampleObject(
+                            value = """
+                                    {
+                                        "firstName": "John",
+                                        "lastName": "Doe",
+                                        "email": "johndoe@mail.com",
+                                        "password": "Password@1234",
+                                        "acceptTerms": true
+                                    }
+                                    """
+                    )
+            )
+    )
     @PostMapping
     public ResponseEntity<String> handle(@RequestBody @Valid SignUpRequestDTO dto) {
         try {
