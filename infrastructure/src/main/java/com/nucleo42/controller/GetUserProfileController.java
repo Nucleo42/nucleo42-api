370446f0323ce3a9d082b91d1b8549a008e7e3ef
maikella <97262283+maikella@users.noreply.github.com>
2025-02-04 18:41:49 -0300

    feat: add GET endpoint to return information for a specific user

---
+++ infrastructure/src/main/java/com/nucleo42/controller/GetUserProfileController.java
@@ -0,0 +1,40 @@
+package com.nucleo42.controller;
+
+import java.util.UUID;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.nucleo42.FindUserProfileByIdCase;
+import com.nucleo42.UserUseCase;
+import com.nucleo42.dto.response.UserResponse;
+import com.nucleo42.exception.UserDoesNotExistException;
+import com.nucleo42.mapper.UserMapper;
+
+@RestController
+@RequestMapping("user")
+public class GetUserProfileController {
+    private final UserMapper userMapper;
+    private final FindUserProfileByIdCase findUserProfileByIdCase;
+
+    public GetUserProfileController(UserMapper userMapper, FindUserProfileByIdCase findUserProfileByIdCase) {
+        this.userMapper = userMapper;
+        this.findUserProfileByIdCase = findUserProfileByIdCase;
+    }
+
+
+    @GetMapping("{id}")
+    public ResponseEntity<?> getUserProfileById(@PathVariable UUID id) {
+        UserResponse user;
+        try {
+            user = userMapper.toResponse(findUserProfileByIdCase.findById(id));
+        } catch (UserDoesNotExistException e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+        }
+        return ResponseEntity.status(HttpStatus.CREATED).body(user);
+    }
+}
