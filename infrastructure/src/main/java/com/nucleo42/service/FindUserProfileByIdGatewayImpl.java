94444534bd8a427e3f170b5dc15a4177af88cd42
maikella <97262283+maikella@users.noreply.github.com>
2025-02-05 17:46:11 -0300

    refactor: update FindUserProfileByIdGatewayImpl

--- infrastructure/src/main/java/com/nucleo42/service/FindUserProfileByIdGatewayImpl.java
+++ infrastructure/src/main/java/com/nucleo42/service/FindUserProfileByIdGatewayImpl.java
@@ -6,9 +6,10 @@ import org.springframework.stereotype.Service;
 
 import com.nucleo42.gateway.FindUserProfileByIdGateway;
 import com.nucleo42.entity.User;
+import com.nucleo42.entity.UserEntity;
+import com.nucleo42.exception.UserDoesNotExistException;
 import com.nucleo42.mapper.UserMapper;
 import com.nucleo42.repository.UserEntityRepository;
-import com.nucleo42.infrastructure.entity.UserEntity;
 
 @Service
 public class FindUserProfileByIdGatewayImpl implements FindUserProfileByIdGateway {
@@ -22,9 +23,11 @@ public class FindUserProfileByIdGatewayImpl implements FindUserProfileByIdGatewa
 
     @Override
     public User findUserById(UUID id) {
-        UserEntity userEntity = userRepository.findById(id);
+        UserEntity userEntity = userRepository.findById(id).orElseThrow(
+            () -> new UserDoesNotExistException()
+        );
+
         User user = userMapper.toUser(userEntity);
-        user.setSkills(userMapper.toSkill(userEntity.getSkills()));
         return user;
     }
 
