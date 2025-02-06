f09befe45fc53e936ebc98746a403d821b93437d
Mikhael D'Amato <damato578@gmail.com>
2025-02-06 11:27:01 -0300

    refactor: update toString method structure of Role from core

--- core/src/main/java/com/nucleo42/entity/Role.java
+++ core/src/main/java/com/nucleo42/entity/Role.java
@@ -44,9 +44,9 @@ public class Role {
 
     @Override
     public String toString() {
-        return "Role{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                '}';
+        return "Role {" +
+                "\n\tid=" + id +
+                ", \n\tname='" + name + '\'' +
+                "\n}";
     }
 }
