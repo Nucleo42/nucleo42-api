7232743982a84a25aae898302b6a8ea3e5c76d5a
matheusgondra <matheusgondra951@gmail.com>
2025-02-07 19:04:29 -0300

    feat: ensure LoginImpl call HashCompare with correct values

--- application/src/main/java/com/nucleo42/application/usecase/LoginImpl.java
+++ application/src/main/java/com/nucleo42/application/usecase/LoginImpl.java
@@ -1,15 +1,20 @@
 package com.nucleo42.application.usecase;
 
 import com.nucleo42.application.gateway.LoadUserByEmailGateway;
+import com.nucleo42.application.protocol.HashCompare;
 import com.nucleo42.exception.InvalidCredentialsException;
 import com.nucleo42.usecase.Login;
 
 public class LoginImpl implements Login {
     private LoadUserByEmailGateway loadUserByEmailGateway;
+    private HashCompare hashCompare;
 
     @Override
     public String login(String email, String password) {
-        this.loadUserByEmailGateway.load(email).orElseThrow(InvalidCredentialsException::new);
+        var user =this.loadUserByEmailGateway.load(email).orElseThrow(InvalidCredentialsException::new);
+
+        this.hashCompare.compare(password, user.getPassword());
+
         return "";
     }
 }
