9f823dbe4cb7d0b7891a3edc29053efa6988f1af
matheusgondra <matheusgondra951@gmail.com>
2025-02-08 11:47:43 -0300

    test: ensure BCryptAdapter.compare call BCrypt.checkpw with correct values

--- infrastructure/src/test/java/com/nucleo42/infrastruture/adapter/BCryptAdapterTest.java
+++ infrastructure/src/test/java/com/nucleo42/infrastruture/adapter/BCryptAdapterTest.java
@@ -1,6 +1,7 @@
 package com.nucleo42.infrastruture.adapter;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.mindrot.jbcrypt.BCrypt;
 
@@ -10,31 +11,52 @@ import static org.mockito.Mockito.mockStatic;
 
 class BCryptAdapterTest {
     private final BCryptAdapter sut = new BCryptAdapter();
-    private final String value = "any_value";
+    private final String valueTest = "any_value";
+    private final String hashedValueTest = "hashed_value";
 
-    @Test
-    @DisplayName("Should call BCrypt.hashpw with correct values")
-    void test01() {
-        try (var mockedBcrypt = mockStatic(BCrypt.class)) {
-            mockedBcrypt.when(() -> BCrypt.gensalt(anyInt())).thenReturn("salt");
-            mockedBcrypt.when(() -> BCrypt.hashpw(anyString(), anyString())).thenReturn("hashed_value");
+    @Nested
+    @DisplayName("hash method")
+    class Hash {
+        @Test
+        @DisplayName("Should call BCrypt.hashpw with correct values")
+        void test01() {
+            try (var mockedBcrypt = mockStatic(BCrypt.class)) {
+                mockedBcrypt.when(() -> BCrypt.gensalt(anyInt())).thenReturn("salt");
+                mockedBcrypt.when(() -> BCrypt.hashpw(anyString(), anyString())).thenReturn("hashed_value");
 
-            sut.hash(value);
+                sut.hash(valueTest);
 
-            mockedBcrypt.verify(() -> BCrypt.hashpw(value, "salt"));
+                mockedBcrypt.verify(() -> BCrypt.hashpw(valueTest, "salt"));
+            }
         }
+
+        @Test
+        @DisplayName("Should return hashed value")
+        void test02() {
+            try (var mockedBcrypt = mockStatic(BCrypt.class)) {
+                mockedBcrypt.when(() -> BCrypt.gensalt(anyInt())).thenReturn("salt");
+                mockedBcrypt.when(() -> BCrypt.hashpw(anyString(), anyString())).thenReturn("hashed_value");
+
+                var result = sut.hash(valueTest);
+
+                assert result.equals("hashed_value");
+            }
+    }
     }
 
-    @Test
-    @DisplayName("Should return hashed value")
-    void test02() {
-        try (var mockedBcrypt = mockStatic(BCrypt.class)) {
-            mockedBcrypt.when(() -> BCrypt.gensalt(anyInt())).thenReturn("salt");
-            mockedBcrypt.when(() -> BCrypt.hashpw(anyString(), anyString())).thenReturn("hashed_value");
+    @Nested
+    @DisplayName("compare method")
+    class Compare {
+        @Test
+        @DisplayName("Should call BCrypt.checkpw with correct values")
+        void test01() {
+            try (var mockedBcrypt = mockStatic(BCrypt.class)) {
+                mockedBcrypt.when(() -> BCrypt.checkpw(anyString(), anyString())).thenReturn(true);
 
-            var result = sut.hash(value);
+                sut.compare(valueTest, hashedValueTest);
 
-            assert result.equals("hashed_value");
+                mockedBcrypt.verify(() -> BCrypt.checkpw(valueTest, hashedValueTest));
+            }
         }
     }
 }
