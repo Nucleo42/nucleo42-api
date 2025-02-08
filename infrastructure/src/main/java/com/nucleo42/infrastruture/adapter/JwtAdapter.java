c6a7a4e36d189ab7c44e9696b51781207dc2e770
matheusgondra <matheusgondra951@gmail.com>
2025-02-08 12:25:24 -0300

    feat: ensure JwtAdapter call JWT.sign with correct value

---
+++ infrastructure/src/main/java/com/nucleo42/infrastruture/adapter/JwtAdapter.java
@@ -0,0 +1,17 @@
+package com.nucleo42.infrastruture.adapter;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.nucleo42.application.protocol.TokenGenerator;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+public class JwtAdapter implements TokenGenerator {
+    private final String secret;
+    private final Algorithm algorithm;
+
+    @Override
+    public String generate(String payload) {
+        return JWT.create().sign(algorithm);
+    }
+}
