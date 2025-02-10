b493ebcb03a0adca41c770bd4aa2ee79971c2203
maikella <97262283+maikella@users.noreply.github.com>
2025-02-10 16:06:47 -0300

    refactor: add spring validation annotations to UpdateUserRequest attributes for validation

--- infrastructure/src/main/java/com/nucleo42/dto/request/UpdateUserRequest.java
+++ infrastructure/src/main/java/com/nucleo42/dto/request/UpdateUserRequest.java
@@ -1,15 +1,19 @@
 package com.nucleo42.dto.request;
 
-import java.util.List;
-
-import com.nucleo42.entity.Skill;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 
 public record UpdateUserRequest(
+        @NotBlank(message = "The 'firstName' field is required")
         String firstName,
+        @NotBlank(message = "The 'lastName' field is required")
         String lastName,
+        @NotBlank(message = "The 'email' field is required")
+        @Email(message = "Invalid email")
         String email,
+        @NotBlank(message = "The 'password' field is required")
         String password,
-        String biography,
-        List<Skill> skills) {
-    
+        @NotBlank(message = "The 'biography' field is required")
+        String biography
+        ){    
 }
