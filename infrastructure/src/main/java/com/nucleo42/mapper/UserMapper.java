8a702acac67e868fc57a6e9519e15535e93f1dcc
maikella <97262283+maikella@users.noreply.github.com>
2025-02-10 15:57:50 -0300

    refactor: making methods of the UserMapper class static

--- infrastructure/src/main/java/com/nucleo42/mapper/UserMapper.java
+++ infrastructure/src/main/java/com/nucleo42/mapper/UserMapper.java
@@ -5,7 +5,6 @@ import java.util.List;
 
 import org.springframework.stereotype.Component;
 
-import com.nucleo42.dto.request.CreateUserRequest;
 import com.nucleo42.dto.request.UpdateUserRequest;
 import com.nucleo42.dto.response.UserResponse;
 import com.nucleo42.entity.Skill;
@@ -16,20 +15,11 @@ import com.nucleo42.entity.UserEntity;
 @Component
 public class UserMapper {
 
-    public User toUser(CreateUserRequest request) {
-        return new User(
-                request.firstName(),
-                request.lastName(),
-                request.email(),
-                request.password(),
-                request.biography(),
-                request.skills());
-    }
-
-    public User toUser(UserEntity userEntity) {
+    public static User toUser(UserEntity userEntity) {
         List<Skill> skills = new ArrayList<>();
         skills = toSkill(userEntity.getSkills());
         return new User(
+                userEntity.getId(),
                 userEntity.getFirstName(),
                 userEntity.getLastName(),
                 userEntity.getEmail(),
@@ -38,17 +28,17 @@ public class UserMapper {
                 skills);
     }
 
-    public User toUser(UpdateUserRequest userRequest) {
-        return new User(
-                userRequest.firstName(),
-                userRequest.lastName(),
-                userRequest.email(),
-                userRequest.password(),
-                userRequest.biography(),
-                userRequest.skills());
+    public static User toUser(UpdateUserRequest request) {
+        User user = new User();
+        user.setFirstName(request.firstName());
+        user.setLastName(request.lastName());
+        user.setEmail(request.email());
+        user.setPassword(request.password());
+        user.setBiography(request.biography());
+        return user;
     }
 
-    public UserEntity toEntity(User user) {
+    public static UserEntity toEntity(User user) {
         UserEntity userEntity = new UserEntity();
         List<SkillEntity> skillEntity = new ArrayList<>();
 
@@ -64,22 +54,12 @@ public class UserMapper {
         return userEntity;
     }
 
-    public UserResponse toResponse(User user) {
+    public static UserResponse toResponse(User user) {
         return new UserResponse(user.getFirstName(), user.getLastName(), user.getEmail(), user.getBiography(),
                 user.getSkills());
     }
 
-    public List<UserResponse> toResponse(List<User> users) {
-        List<UserResponse> usersR = new ArrayList<>();
-
-        for (User u : users) {
-            usersR.add(
-                    new UserResponse(u.getFirstName(), u.getLastName(), u.getEmail(), u.getBiography(), u.getSkills()));
-        }
-        return usersR;
-    }
-
-    private List<SkillEntity> toSkillEntity(List<Skill> skills) {
+    public static List<SkillEntity> toSkillEntity(List<Skill> skills) {
         List<SkillEntity> skillEntities = new ArrayList<>();
         for (Skill s : skills) {
             SkillEntity skillEntity = new SkillEntity();
@@ -89,7 +69,7 @@ public class UserMapper {
         return skillEntities;
     }
 
-    private List<Skill> toSkill(List<SkillEntity> skillEntities) {
+    private static List<Skill> toSkill(List<SkillEntity> skillEntities) {
         List<Skill> skills = new ArrayList<>();
         for (SkillEntity s : skillEntities) {
             skills.add(new Skill(s.getName()));
