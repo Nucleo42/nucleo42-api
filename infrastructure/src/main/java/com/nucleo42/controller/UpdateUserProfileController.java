defff417fac8c2e79cdc301de9d99219b93c43bb
maikella <97262283+maikella@users.noreply.github.com>
2025-02-10 16:01:08 -0300

    refactor: update UpdateUserProfileController class

--- infrastructure/src/main/java/com/nucleo42/controller/UpdateUserProfileController.java
+++ infrastructure/src/main/java/com/nucleo42/controller/UpdateUserProfileController.java
@@ -2,42 +2,35 @@ package com.nucleo42.controller;
 
 import java.util.UUID;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.nucleo42.FindUserProfileByIdCase;
 import com.nucleo42.UpdateUserProfileCase;
 import com.nucleo42.dto.request.UpdateUserRequest;
 import com.nucleo42.entity.User;
 import com.nucleo42.exception.UserDoesNotExistException;
 import com.nucleo42.mapper.UserMapper;
 
-import io.swagger.v3.oas.annotations.parameters.RequestBody;
+import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("user/update")
 public class UpdateUserProfileController {
-    private final FindUserProfileByIdCase findUserCase;
-    private final UserMapper userMapper;
-    private final UpdateUserProfileCase updateUserCase;
-
-    public UpdateUserProfileController(FindUserProfileByIdCase findUserCase, UserMapper userMapper,
-            UpdateUserProfileCase updateUserCase) {
-        this.findUserCase = findUserCase;
-        this.userMapper = userMapper;
-        this.updateUserCase = updateUserCase;
-    }
+    @Autowired
+    private UpdateUserProfileCase updateUserCase;
 
     @PutMapping("{id}")
-    public ResponseEntity<?> updateUserProfileById(@PathVariable UUID id, @RequestBody UpdateUserRequest updateUser) {
+    public ResponseEntity<?> updateUserProfileById(@PathVariable UUID id,
+            @RequestBody @Valid UpdateUserRequest updateUser) {
         try {
-            User user = findUserCase.findById(id);
-            user = userMapper.toUser(updateUser);
-            updateUserCase.update(user);
+            User user = UserMapper.toUser(updateUser);
+            updateUserCase.update(user, id);
         } catch (UserDoesNotExistException e) {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
         }
