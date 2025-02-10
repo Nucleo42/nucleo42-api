c481ed2731a1e900ed4333c6ea9f20b80c386a30
Mikhael D'Amato <damato578@gmail.com>
2025-02-09 21:27:21 -0300

    test: ensures creation of project when POST ProjectController create

---
+++ infrastructure/src/test/java/com/nucleo42/infrastruture/controller/ProjectControllerTest.java
@@ -0,0 +1,51 @@
+package com.nucleo42.infrastruture.controller;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.nucleo42.infrastruture.dto.CreateProjectRequestDTO;
+import com.nucleo42.infrastruture.repository.IProjectEntityRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@ActiveProfiles("test")
+class ProjectControllerTest {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private IProjectEntityRepository projectEntityRepository;
+
+    @Autowired
+    ObjectMapper objectMapper;
+
+    private CreateProjectRequestDTO createProjectRequestDTO;
+
+    @BeforeEach
+    void setup() {
+        createProjectRequestDTO = new CreateProjectRequestDTO("NucleoProject", "Its a Project", 12, "Make a project");
+        projectEntityRepository.deleteAll();
+    }
+
+    @Test
+    @DisplayName("Should create a project and return status 201 on success")
+    void test01() throws Exception {
+        mockMvc.perform(post("/project")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(createProjectRequestDTO)))
+                .andExpect(status().isCreated());
+    }
+}
