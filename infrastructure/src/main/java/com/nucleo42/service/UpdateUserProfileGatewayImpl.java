615a6e156e29d358afe245a11076e1df45027f81
maikella <97262283+maikella@users.noreply.github.com>
2025-02-10 21:09:50 -0300

    refactor: update UpdateUserProfileGatewayImpl

--- infrastructure/src/main/java/com/nucleo42/service/UpdateUserProfileGatewayImpl.java
+++ infrastructure/src/main/java/com/nucleo42/service/UpdateUserProfileGatewayImpl.java
@@ -1,26 +1,48 @@
 package com.nucleo42.service;
 
+import java.util.List;
+import java.util.UUID;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.nucleo42.gateway.UpdateUserProfileGateway;
+import com.nucleo42.entity.SkillEntity;
 import com.nucleo42.entity.User;
 import com.nucleo42.mapper.UserMapper;
 import com.nucleo42.repository.UserEntityRepository;
+
 import com.nucleo42.entity.UserEntity;
+import com.nucleo42.exception.UserDoesNotExistException;
 
 @Service
-public class UpdateUserProfileGatewayImpl implements UpdateUserProfileGateway{
-    private final UserEntityRepository userRepository;
-    private final UserMapper userMapper;
-
-    public UpdateUserProfileGatewayImpl(UserEntityRepository userRepository, UserMapper userMapper) {
-        this.userRepository = userRepository;
-        this.userMapper = userMapper;
-    }
+public class UpdateUserProfileGatewayImpl implements UpdateUserProfileGateway {
+    @Autowired
+    private UserEntityRepository userRepository;
 
     @Override
-    public void update(User user) {
-        UserEntity userEntity = userMapper.toEntity(user);
+    public void update(User userUpdate, UUID id) {
+
+        UserEntity userEntity = findUserById(id);
+        userEntity = updateUserEntity(userEntity, userUpdate);
         userRepository.save(userEntity);
+
     }
+
+    public UserEntity findUserById(UUID id) {
+        return userRepository.findById(id).orElseThrow(
+                () -> new UserDoesNotExistException());
+    }
+
+    private UserEntity updateUserEntity(UserEntity user, User userUpdate) {
+
+        user.setFirstName(userUpdate.getFirstName());
+        user.setLastName(userUpdate.getLastName());
+        user.setEmail(userUpdate.getEmail());
+        user.setPassword(userUpdate.getPassword());
+        user.setBiography(userUpdate.getBiography());
+
+        return user;
+    }
+
 }
