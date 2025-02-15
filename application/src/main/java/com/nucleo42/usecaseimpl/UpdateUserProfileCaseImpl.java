11e6380edce7c4034aa100012c403e024f5675f5
maikella <97262283+maikella@users.noreply.github.com>
2025-02-15 15:20:33 -0300

    refactor: update UpdateUserProfileCaseImpl

--- application/src/main/java/com/nucleo42/usecaseimpl/UpdateUserProfileCaseImpl.java
+++ application/src/main/java/com/nucleo42/usecaseimpl/UpdateUserProfileCaseImpl.java
@@ -1,10 +1,13 @@
 package com.nucleo42.usecaseimpl;
 
+import java.util.UUID;
+
 import com.nucleo42.UpdateUserProfileCase;
 import com.nucleo42.entity.User;
 import com.nucleo42.gateway.UpdateUserProfileGateway;
 
 public class UpdateUserProfileCaseImpl implements UpdateUserProfileCase {
+
     private final UpdateUserProfileGateway updateGateway;
 
     public UpdateUserProfileCaseImpl(UpdateUserProfileGateway updateGateway) {
@@ -12,7 +15,7 @@ public class UpdateUserProfileCaseImpl implements UpdateUserProfileCase {
     }
 
     @Override
-    public void update(User user) {
-        updateGateway.update(user);
+    public void update(User userUpdate, UUID id) {
+        updateGateway.update(userUpdate, id);
     }
 }
