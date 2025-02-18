9ba971b01abca2fa9fc9e137cbfddf34a449b4e6
Mikhael D'Amato <damato578@gmail.com>
2025-02-18 14:15:38 -0300

    feat: implements FindAllProjectsGateway in ProjectGateway

--- infrastructure/src/main/java/com/nucleo42/infrastruture/service/ProjectGateway.java
+++ infrastructure/src/main/java/com/nucleo42/infrastruture/service/ProjectGateway.java
@@ -1,14 +1,17 @@
 package com.nucleo42.infrastruture.service;
 
 import com.nucleo42.application.gateway.AddProjectGateway;
+import com.nucleo42.application.gateway.FindAllProjectsGateway;
 import com.nucleo42.entity.Project;
 import com.nucleo42.infrastruture.mapper.ProjectMapper;
 import com.nucleo42.infrastruture.repository.ProjectRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
-public class ProjectGateway implements AddProjectGateway {
+public class ProjectGateway implements AddProjectGateway, FindAllProjectsGateway {
 
     @Autowired
     private ProjectRepository projectEntityRepository;
@@ -17,6 +20,11 @@ public class ProjectGateway implements AddProjectGateway {
     public Boolean create(Project project) {
         projectEntityRepository.save(ProjectMapper.toEntity(project));
         return true;
+    }
+    
+    @Override
+    public List<Project> findAll() {
+        return projectEntityRepository.findAll().stream().map(ProjectMapper::fromEntity).toList();
     }
 }
 
