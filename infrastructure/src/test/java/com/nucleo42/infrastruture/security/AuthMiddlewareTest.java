53dae26a6f6caf1e07ec87de71aebc73ef4de9cd
matheusgondra <matheusgondra951@gmail.com>
2025-02-18 20:57:24 -0300

    test: ensure AuthMiddleware allow public routes

---
+++ infrastructure/src/test/java/com/nucleo42/infrastruture/security/AuthMiddlewareTest.java
@@ -0,0 +1,32 @@
+package com.nucleo42.infrastruture.security;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.mock.web.MockFilterChain;
+import org.springframework.mock.web.MockHttpServletRequest;
+import org.springframework.mock.web.MockHttpServletResponse;
+import org.springframework.test.context.ActiveProfiles;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@ActiveProfiles("test")
+@ExtendWith(MockitoExtension.class)
+class AuthMiddlewareTest {
+    @InjectMocks
+    private AuthMiddleware sut;
+
+    private MockHttpServletRequest request = new MockHttpServletRequest();
+    private MockHttpServletResponse response = new MockHttpServletResponse();
+    private MockFilterChain filterChain = new MockFilterChain();
+
+    @Test
+    @DisplayName("Should allow public routes")
+    void test01() throws Exception {
+        request.setRequestURI("/signup");
+        sut.doFilterInternal(request, response, filterChain);
+        assertEquals(200, response.getStatus());
+    }
+}
