4d261bb11e7ba286f1cccb89f726744789960f67
maikella <97262283+maikella@users.noreply.github.com>
2025-02-18 23:02:30 -0300

    refactor: update UpdateUserProfileCase

--- usecase/src/main/java/com/nucleo42/UpdateUserProfileCase.java
+++ usecase/src/main/java/com/nucleo42/UpdateUserProfileCase.java
@@ -1,7 +1,9 @@
 package com.nucleo42;
 
+import java.util.UUID;
+
 import com.nucleo42.entity.User;
 
 public interface UpdateUserProfileCase {
-    void update(User user);
+    void update(User user, UUID id);
 }
