4365fee1bd75a0a95be97f83bcd90b8a305c4340
matheusgondra <matheusgondra951@gmail.com>
2025-02-18 22:08:20 -0300

    test: ensure JwtAdapter.decode call JWT.verify with correct value

--- infrastructure/src/test/java/com/nucleo42/infrastruture/adapter/JwtAdapterTest.java
+++ infrastructure/src/test/java/com/nucleo42/infrastruture/adapter/JwtAdapterTest.java
@@ -2,11 +2,11 @@ package com.nucleo42.infrastruture.adapter;
 
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.JWTCreator;
+import com.auth0.jwt.JWTVerifier;
 import com.auth0.jwt.algorithms.Algorithm;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import com.auth0.jwt.interfaces.DecodedJWT;
+import com.auth0.jwt.interfaces.Verification;
+import org.junit.jupiter.api.*;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.MockedStatic;
 import org.mockito.junit.jupiter.MockitoExtension;
@@ -23,6 +23,9 @@ class JwtAdapterTest {
     private final String secretTest = "secretTest";
     private final String payloadTest = "test";
     private MockedStatic<JWT> mockedJWT;
+    private Verification mockedVerification;
+    private JWTVerifier mockedVerifier;
+    private DecodedJWT mockedDecodedJWT;
     private JWTCreator.Builder mockedBuild;
 
 
@@ -31,12 +34,19 @@ class JwtAdapterTest {
         sut = new JwtAdapter(secretTest, algorithm);
         mockedJWT = mockStatic(JWT.class);
         mockedBuild = mock(JWTCreator.Builder.class);
+        mockedVerification = mock(Verification.class);
+        mockedVerifier = mock(JWTVerifier.class);
+        mockedDecodedJWT = mock(DecodedJWT.class);
+
 
         mockedJWT.when(JWT::create).thenReturn(mockedBuild);
-        when(mockedBuild.withIssuer(anyString())).thenReturn(mockedBuild);
-        when(mockedBuild.sign(any(Algorithm.class))).thenReturn("tokenTest");
-        when(mockedBuild.withSubject(anyString())).thenReturn(mockedBuild);
-        when(mockedBuild.withExpiresAt(any(Instant.class))).thenReturn(mockedBuild);
+        lenient().when(mockedBuild.withIssuer(anyString())).thenReturn(mockedBuild);
+        lenient().when(mockedBuild.sign(any(Algorithm.class))).thenReturn("tokenTest");
+        lenient().when(mockedBuild.withSubject(anyString())).thenReturn(mockedBuild);
+        lenient().when(mockedBuild.withExpiresAt(any(Instant.class))).thenReturn(mockedBuild);
+        lenient().when(JWT.require(algorithm)).thenReturn(mockedVerification);
+        lenient().when(mockedVerification.build()).thenReturn(mockedVerifier);
+        lenient().when(mockedVerifier.verify("tokenTest")).thenReturn(mockedDecodedJWT);
     }
 
     @AfterEach
@@ -44,40 +54,54 @@ class JwtAdapterTest {
         mockedJWT.close();
     }
 
-    @Test
-    @DisplayName("Should call JWT.sign with correct value")
-    void test01() {
+    @Nested
+    @DisplayName("generate method")
+    class GenerateTest {
+        @Test
+        @DisplayName("Should call JWT.sign with correct value")
+        void test01() {
+                sut.generate(payloadTest);
+                verify(mockedBuild).sign(algorithm);
+        }
+
+        @Test
+        @DisplayName("Should sign token with issuer nucleo42")
+        void test02() {
+                sut.generate(payloadTest);
+                verify(mockedBuild).withIssuer("nucleo42");
+        }
+
+        @Test
+        @DisplayName("Should sign token with correct subject")
+        void test03() {
+                sut.generate(payloadTest);
+                verify(mockedBuild).withSubject(payloadTest);
+        }
+
+        @Test
+        @DisplayName("Should sign token with correct expiration")
+        void test04() {
             sut.generate(payloadTest);
-            verify(mockedBuild).sign(algorithm);
+            var expiresAt = Instant.now().plus(Duration.ofHours(72));
+            verify(mockedBuild).withExpiresAt(expiresAt);
+        }
 
+        @Test
+        @DisplayName("Should return token")
+        void test05() {
+            var token = sut.generate(payloadTest);
+            assert token.equals("tokenTest");
+        }
     }
 
-    @Test
-    @DisplayName("Should sign token with issuer nucleo42")
-    void test02() {
-            sut.generate(payloadTest);
-            verify(mockedBuild).withIssuer("nucleo42");
-    }
-
-    @Test
-    @DisplayName("Should sign token with correct subject")
-    void test03() {
-            sut.generate(payloadTest);
-            verify(mockedBuild).withSubject(payloadTest);
-    }
-
-    @Test
-    @DisplayName("Should sign token with correct expiration")
-    void test04() {
-        sut.generate(payloadTest);
-        var expiresAt = Instant.now().plus(Duration.ofHours(72));
-        verify(mockedBuild).withExpiresAt(expiresAt);
-    }
-
-    @Test
-    @DisplayName("Should return token")
-    void test05() {
-        var token = sut.generate(payloadTest);
-        assert token.equals("tokenTest");
+    @Nested
+    @DisplayName("decode method")
+    class DecodeTest {
+        @Test
+        @DisplayName("Should call JWT.verify with correct value")
+        void test01() {
+            sut.decode("tokenTest");
+            verify(mockedVerifier).verify("tokenTest");
+        }
     }
 }
