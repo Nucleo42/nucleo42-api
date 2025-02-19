6d160a9e19c0661fd1f1a307e1b214941133ffbe
maikella <97262283+maikella@users.noreply.github.com>
2025-02-18 22:37:29 -0300

    feat: add skill enum

---
+++ infrastructure/src/main/java/com/nucleo42/dto/request/SkillEnum.java
@@ -0,0 +1,39 @@
+package com.nucleo42.dto.request;
+
+import com.nucleo42.exception.SkillDoesNotExistException;
+
+public enum SkillEnum {
+    COMUNICACAO(1, "Comunicação"),
+    TRABALHO_EQUIPE(2, "Trabalho em equipe"),
+    ANGULAR(3, "Angular"),
+    JAVA(4, "Java"),
+    RESOLUCAO_PROBLEMA(5, "Resolução de problemas"),
+    JAVASCRIPT(6, "Javascript"),
+    TYPESCRIPT(7, "Typescript");
+
+    private long id;
+    private String skill;
+
+    SkillEnum(long id, String skill) {
+        this.id = id;
+        this.skill = skill;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getSkill() {
+        return skill;
+    }
+
+    public static String fromId(long id) {
+        for (SkillEnum s : SkillEnum.values()) {
+            if (s.getId() == id) {
+                return s.getSkill();
+            }
+        }
+        throw new SkillDoesNotExistException(id);
+    }
+
+}
