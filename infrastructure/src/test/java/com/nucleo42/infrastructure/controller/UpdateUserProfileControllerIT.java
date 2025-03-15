2695afaed608b35351a05b1e7b1755ac1faf4474
maikella <97262283+maikella@users.noreply.github.com>
2025-03-15 19:20:40 -0300

    refactor: update UpdateUserProfileControllerIT

--- infrastructure/src/test/java/com/nucleo42/infrastructure/controller/UpdateUserProfileControllerIT.java
+++ infrastructure/src/test/java/com/nucleo42/infrastructure/controller/UpdateUserProfileControllerIT.java
@@ -1,50 +1,69 @@
 package com.nucleo42.infrastructure.controller;
 
-import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-import java.time.LocalDateTime;
-import java.util.Optional;
-import java.util.UUID;
-
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.nucleo42.dto.request.UpdateUserRequestDTO;
 import com.nucleo42.entity.UserEntity;
-import com.nucleo42.exception.UserDoesNotExistException;
 import com.nucleo42.repository.UserEntityRepository;
-import com.nucleo42.service.UpdateUserProfileGatewayImpl;
 
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 @AutoConfigureMockMvc
 @ActiveProfiles("test")
 public class UpdateUserProfileControllerIT {
-    @Mock
+    @Autowired
     private UserEntityRepository userRepository;
-    @InjectMocks
-    private UpdateUserProfileGatewayImpl updateService;
+    @Autowired
+    private MockMvc mockMvc;
+    @Autowired
     private UserEntity userEntity;
-    // private UUID id = UUID.randomUUID();
+    @Autowired
+    private ObjectMapper objectMapper;
+    private UpdateUserRequestDTO userRequest;
+    private UpdateUserRequestDTO userNotExistRequest;
 
     @BeforeEach
     void setup() {
         userRepository.deleteAll();
 
-        userEntity = new UserEntity(null, "Núcleo", "42", "nucleo42@gmail.com", "Nova senha", true,
-                false,
-                "Biografia", null, LocalDateTime.now(), LocalDateTime.now());
+        userEntity = new UserEntity(null, "Núcleo", "42", "nucleo42@gmail.com", "Nova senha", true, false, "Biografia",
+                null, null, null, null);
+        userEntity = userRepository.save(userEntity);
+
+        userRequest = new UpdateUserRequestDTO(userEntity.getId().toString(), "Núcleo2", "422", "nucleo422@gmail.com", "Nova senha2", " ");
+
+        userNotExistRequest = new UpdateUserRequestDTO("61776d3c-ceaf-4cd0-959c-794ac86f3732", "Núcleo2", "422", "nucleo422@gmail.com", "Nova senha2", " ");
     }
 
     @Test
-    void userProfileExists() {
-        when(userRepository.findById(userEntity.getId()))
-                .thenReturn(Optional.of(userEntity));
+    @DisplayName("Should return code 200 and the user has been updated successfully")
+    void updateUserProfileCase01() throws JsonProcessingException, Exception {
+        mockMvc.perform(put("/user/update")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(userRequest)))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    @DisplayName("Should return code 404 if the user was updated successfully")
+    void updateUserProfileCase02() throws JsonProcessingException, Exception {
+        mockMvc.perform(put("/user/update")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(userNotExistRequest)))
+                .andExpect(status().isNotFound());
     }
 
 }
