9764f98fd239858120df80d11b0cc9fb65dd3972
maikella <97262283+maikella@users.noreply.github.com>
2025-03-16 16:49:23 -0300

    refactor: update UpdateUserProfileController

--- infrastructure/src/main/java/com/nucleo42/controller/UpdateUserProfileController.java
+++ infrastructure/src/main/java/com/nucleo42/controller/UpdateUserProfileController.java
@@ -1,18 +1,15 @@
 package com.nucleo42.controller;
 
-import java.util.UUID;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.nucleo42.UpdateUserProfileCase;
-import com.nucleo42.dto.request.UpdateUserRequest;
+import com.nucleo42.dto.request.UpdateUserRequestDTO;
 import com.nucleo42.entity.User;
 import com.nucleo42.exception.SkillDoesNotExistException;
 import com.nucleo42.exception.UserDoesNotExistException;
@@ -21,17 +18,16 @@ import com.nucleo42.mapper.UserMapper;
 import jakarta.validation.Valid;
 
 @RestController
-@RequestMapping("user/update")
+@RequestMapping("/user/update")
 public class UpdateUserProfileController {
     @Autowired
     private UpdateUserProfileCase updateUserCase;
 
-    @PutMapping("{id}")
-    public ResponseEntity<?> updateUserProfileById(@PathVariable UUID id,
-            @RequestBody @Valid UpdateUserRequest updateUser) {
+    @PutMapping
+    public ResponseEntity<?> updateUserProfileById(@RequestBody @Valid UpdateUserRequestDTO updateUser) {
         try {
             User user = UserMapper.toUser(updateUser);
-            updateUserCase.update(user, id);
+            updateUserCase.update(user);
         } catch (UserDoesNotExistException | SkillDoesNotExistException e) {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
         }
