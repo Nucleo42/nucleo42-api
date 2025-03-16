7db62437c9c3e4b0a3326cb8273b648fd0e6db7e
maikella <97262283+maikella@users.noreply.github.com>
2025-03-16 16:39:44 -0300

    update UserMapper

--- infrastructure/src/main/java/com/nucleo42/mapper/UserMapper.java
+++ infrastructure/src/main/java/com/nucleo42/mapper/UserMapper.java
@@ -1,56 +1,64 @@
 package com.nucleo42.mapper;
 
-import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
 
 import org.springframework.stereotype.Component;
 
-import com.nucleo42.dto.request.SkillRequest;
-import com.nucleo42.dto.request.UpdateUserRequest;
+import com.nucleo42.dto.request.UpdateUserRequestDTO;
 import com.nucleo42.dto.response.UserResponse;
 import com.nucleo42.entity.Skill;
 import com.nucleo42.entity.SkillEntity;
-import com.nucleo42.dto.request.SkillEnum;
 import com.nucleo42.entity.User;
 import com.nucleo42.entity.UserEntity;
+import com.nucleo42.exception.UserIdIsInvalidException;
 
 @Component
 public class UserMapper {
 
     public static User toUser(UserEntity userEntity) {
-        List<Skill> skills = new ArrayList<>();
-        skills = toSkill(userEntity.getSkills());
+        List<Skill> skills = SkillMapper.toSkill(userEntity.getSkills());
         return new User(
+                userEntity.getId(),
                 userEntity.getFirstName(),
                 userEntity.getLastName(),
                 userEntity.getEmail(),
                 userEntity.getPassword(),
                 userEntity.getBiography(),
+                userEntity.getAceptTerms(),
                 skills);
     }
 
-    public static User toUser(UpdateUserRequest request) {
+    public static User toUser(UpdateUserRequestDTO request) {
+        UUID id = null;
+
+        try {
+            id = UUID.fromString(request.id());
+        } catch (IllegalArgumentException e) {
+            throw new UserIdIsInvalidException(request.id());
+        }
+
         User user = new User();
+        user.setId(id);
         user.setFirstName(request.firstName());
         user.setLastName(request.lastName());
         user.setEmail(request.email());
         user.setPassword(request.password());
         user.setBiography(request.biography());
-        user.setSkills(skillFromId(request.skills()));
         return user;
     }
 
     public static UserEntity toEntity(User user) {
         UserEntity userEntity = new UserEntity();
-        List<SkillEntity> skillEntity = new ArrayList<>();
 
         userEntity.setFirstName(user.getFirstName());
         userEntity.setLastName(user.getLastName());
         userEntity.setEmail(user.getEmail());
         userEntity.setPassword(user.getPassword());
         userEntity.setBiography(user.getBiography());
+        userEntity.setAceptTerms(true);
 
-        skillEntity = toSkillEntity(user.getSkills());
+        List<SkillEntity>skillEntity = SkillMapper.toSkillEntity(user.getSkills());
         userEntity.setSkills(skillEntity);
 
         return userEntity;
@@ -61,31 +69,14 @@ public class UserMapper {
                 user.getSkills());
     }
 
-    public static List<SkillEntity> toSkillEntity(List<Skill> skills) {
-        List<SkillEntity> skillEntities = new ArrayList<>();
-        for (Skill s : skills) {
-            SkillEntity skillEntity = new SkillEntity();
-            skillEntity.setName(s.getName());
-            skillEntities.add(skillEntity);
-        }
-        return skillEntities;
-    }
-
-    private static List<Skill> toSkill(List<SkillEntity> skillEntities) {
-        List<Skill> skills = new ArrayList<>();
-        for (SkillEntity s : skillEntities) {
-            skills.add(new Skill(s.getName()));
-        }
-        return skills;
-    }
-    private static List<Skill> skillFromId(List<SkillRequest> skillsIds){
-        List<Skill> skills = new ArrayList<>();
-        for(SkillRequest skill: skillsIds){
-            Skill s = new Skill();
-            s.setName(SkillEnum.fromId(skill.id()));
-            skills.add(s);
-        }
-        return skills;
+    public static UserEntity updateUserEntity(UserEntity user, User userUpdate) {
+        user.setId(userUpdate.getId());
+        user.setFirstName(userUpdate.getFirstName());
+        user.setLastName(userUpdate.getLastName());
+        user.setEmail(userUpdate.getEmail());
+        user.setPassword(userUpdate.getPassword());
+        user.setBiography(userUpdate.getBiography());
+        return user;
     }
 
 }
