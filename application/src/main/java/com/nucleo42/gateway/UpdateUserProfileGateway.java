f61346d384b5e4c40c943a3214a504a831e3af0c
maikella <97262283+maikella@users.noreply.github.com>
2025-03-16 16:32:47 -0300

    refactor: update UpdateUserProfileGateway

--- application/src/main/java/com/nucleo42/gateway/UpdateUserProfileGateway.java
+++ application/src/main/java/com/nucleo42/gateway/UpdateUserProfileGateway.java
@@ -1,10 +1,9 @@
 package com.nucleo42.gateway;
 
-import java.util.UUID;
 
 import com.nucleo42.entity.User;
 
 public interface UpdateUserProfileGateway {
-    void update(User userUpdate, UUID id);
+    boolean update(User userUpdate);
 
 }
