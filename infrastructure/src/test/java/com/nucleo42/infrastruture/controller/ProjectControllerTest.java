ad58507ddf83592f5d20bfd47a9cc34ea13da599
Mikhael D'Amato <damato578@gmail.com>
2025-03-18 16:44:39 -0300

    test: ensures that a Project will be removed

--- infrastructure/src/test/java/com/nucleo42/infrastruture/controller/ProjectControllerTest.java
+++ infrastructure/src/test/java/com/nucleo42/infrastruture/controller/ProjectControllerTest.java
@@ -13,14 +13,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.util.List;
 import java.util.UUID;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -108,5 +108,12 @@ class ProjectControllerTest {
                 .andExpect(jsonPath("$.size()").value(2))
                 .andExpect(jsonPath("$[0].name").value(projects.getFirst().getName()))
                 .andExpect(jsonPath("$[1].name").value(projects.get(1).getName()));
+    }
+
+    @Test
+    @DisplayName("Should remove a project")
+    void test05() throws Exception {
+        mockMvc.perform(delete("/project/" + projects.getFirst().getId()).contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
     }
 }
