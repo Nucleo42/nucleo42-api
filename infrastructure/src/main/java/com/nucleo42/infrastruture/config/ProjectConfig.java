5145f4c756d166ca155cb170710df3548f9badd8
Mikhael D'Amato <damato578@gmail.com>
2025-03-18 16:49:01 -0300

    feat: add removeProject in ProjectConfig

--- infrastructure/src/main/java/com/nucleo42/infrastruture/config/ProjectConfig.java
+++ infrastructure/src/main/java/com/nucleo42/infrastruture/config/ProjectConfig.java
@@ -2,11 +2,14 @@ package com.nucleo42.infrastruture.config;
 
 import com.nucleo42.application.gateway.AddProjectGateway;
 import com.nucleo42.application.gateway.FindAllProjectsGateway;
+import com.nucleo42.application.gateway.RemoveProjectGateway;
 import com.nucleo42.application.usecase.AddProjectImpl;
 import com.nucleo42.application.usecase.FindAllProjectsImpl;
+import com.nucleo42.application.usecase.RemoveProjectImpl;
 import com.nucleo42.infrastruture.service.ProjectGateway;
 import com.nucleo42.usecase.AddProject;
 import com.nucleo42.usecase.FindAllProjects;
+import com.nucleo42.usecase.RemoveProject;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -27,5 +30,11 @@ public class ProjectConfig {
     @Bean
     public FindAllProjects findAllProjects(FindAllProjectsGateway findAllProjectsGateway) {
         return new FindAllProjectsImpl(findAllProjectsGateway);
+    }
+
+    @Bean
+    public RemoveProject removeProject(RemoveProjectGateway removeProjectGateway)
+    {
+        return new RemoveProjectImpl(removeProjectGateway);
     }
 }
