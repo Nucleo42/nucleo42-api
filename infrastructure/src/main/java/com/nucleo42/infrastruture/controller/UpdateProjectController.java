e6b0f37c27fd7f84fc68cbfe2865fde2103644d3
Mikhael D'Amato <damato578@gmail.com>
2025-04-13 17:44:14 -0300

    docs: documentation to PUT update project in UpdateProjectController

--- infrastructure/src/main/java/com/nucleo42/infrastruture/controller/UpdateProjectController.java
+++ infrastructure/src/main/java/com/nucleo42/infrastruture/controller/UpdateProjectController.java
@@ -1,8 +1,15 @@
 package com.nucleo42.infrastruture.controller;
 
 import com.nucleo42.entity.Project;
+import com.nucleo42.infrastruture.annotation.ApiRequestBody;
 import com.nucleo42.infrastruture.dto.UpdateProjectRequestDTO;
 import com.nucleo42.usecase.UpdateProject;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.ExampleObject;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -12,6 +19,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.ArrayList;
 import java.util.UUID;
 
+@Tag(name="project")
 @AllArgsConstructor
 @RestController
 @RequestMapping("/project")
@@ -19,6 +27,77 @@ public class UpdateProjectController {
 
     private final UpdateProject updateProjectUseCase;
 
+    @Operation(
+            description = "Update a new project",
+            summary = "Update Project"
+    )
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "Project updated",
+                    content = @Content(
+                            mediaType = "text/plain",
+                            examples = @ExampleObject(value = "Project updated successfully")
+                    )
+            ),
+            @ApiResponse(
+                    responseCode = "422",
+                    description = "Unprocessable Entity",
+                    content = @Content(
+                            mediaType = "application/json",
+                            examples = @ExampleObject(
+                                    value = """
+                                            {
+                                                "status": 422,
+                                                "message": "Campos inválidos",
+                                                "errors": [
+                                                    {
+                                                        "field": "goal",
+                                                        "error": "O objetivo do projeto precisa ser informado"
+                                                    },
+                                                    {
+                                                        "field": "name",
+                                                        "error": "O nome do projeto precisa ser informado"
+                                                    },
+                                                    {
+                                                        "field": "description",
+                                                        "error": "A descrição do projeto precisa ser informado"
+                                                    },
+                                                    {
+                                                        "field": "vacancies",
+                                                        "error": "As vagas do projeto precisam ser informadas"
+                                                    }
+                                                ]
+                                            }
+                                            """
+                            )
+
+                    )
+            ),
+            @ApiResponse(
+                    responseCode = "500",
+                    description = "Internal server error",
+                    content = @Content(
+                            mediaType = "text/plain",
+                            examples = @ExampleObject(value = "Internal server error")
+                    )
+            )
+    })
+    @ApiRequestBody(
+            content = @Content(
+                    mediaType = "application/json",
+                    examples = @ExampleObject(
+                            value = """
+                                    {
+                                        "name": "Project Name",
+                                        "description": "Its a project",
+                                        "vacancies": 1,
+                                        "goal": "Make a project"
+                                    }
+                                    """
+                    )
+            )
+    )
     @PutMapping("{id}")
     public ResponseEntity<String> create(@PathVariable("id") String projectId, @RequestBody @Valid UpdateProjectRequestDTO dto)
     {
