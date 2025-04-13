a36bd1cbf09e41d21898858d7039cedb05564238
Mikhael D'Amato <damato578@gmail.com>
2025-04-13 12:43:49 -0300

    docs: documentation to DELETE delete project in RemoveProjectController

--- infrastructure/src/main/java/com/nucleo42/infrastruture/controller/RemoveProjectController.java
+++ infrastructure/src/main/java/com/nucleo42/infrastruture/controller/RemoveProjectController.java
@@ -1,6 +1,13 @@
 package com.nucleo42.infrastruture.controller;
 
 import com.nucleo42.usecase.RemoveProject;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.ExampleObject;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -10,17 +17,40 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.UUID;
 
+@AllArgsConstructor
+@Tag(name = "project")
 @RestController
 @RequestMapping("/project")
 public class RemoveProjectController {
 
     private final RemoveProject removeProject;
 
-    public RemoveProjectController(RemoveProject removeProject)
-    {
-        this.removeProject = removeProject;
-    }
-
+    @Operation(
+            description = "Delete a project",
+            summary = "Delete Project"
+    )
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "Project deleted",
+                    content = @Content(
+                            mediaType = "text/plain",
+                            examples = @ExampleObject(value = "Project deleted successfully")
+                    )
+            ),
+            @ApiResponse(
+                    responseCode = "400",
+                    description = "Bad Request"
+            ),
+            @ApiResponse(
+                    responseCode = "500",
+                    description = "Internal server error",
+                    content = @Content(
+                            mediaType = "text/plain",
+                            examples = @ExampleObject(value = "Internal server error")
+                    )
+            )
+    })
     @DeleteMapping("{id}")
     public ResponseEntity<String> remove(@PathVariable("id") UUID projectId)
     {
