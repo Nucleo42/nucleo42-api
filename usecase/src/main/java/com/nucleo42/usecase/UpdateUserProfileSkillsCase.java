c827e9cde86a7fd309b21eab6571a19bdfa7194b
maikella <97262283+maikella@users.noreply.github.com>
2025-04-15 17:53:25 -0300

    refactor: update UpdateUserProfileSkillsCase

--- usecase/src/main/java/com/nucleo42/usecase/UpdateUserProfileSkillsCase.java
+++ usecase/src/main/java/com/nucleo42/usecase/UpdateUserProfileSkillsCase.java
@@ -1,9 +1,10 @@
 package com.nucleo42.usecase;
 
 import java.util.List;
+import java.util.UUID;
 
 import com.nucleo42.entity.Skill;
 
 public interface UpdateUserProfileSkillsCase {
-    boolean updateSkills(List<Skill> skills, String userId);
+    boolean updateSkills(List<Skill> skills, UUID userId);
 }
