ae6ced0c694fd4e2b6b2df48bf28bb2158ec2cfd
maikella <97262283+maikella@users.noreply.github.com>
2025-04-15 17:23:19 -0300

    refactor: update dtos

--- infrastructure/src/main/java/com/nucleo42/infrastructure/dto/SkillRequestDTO.java
+++ infrastructure/src/main/java/com/nucleo42/infrastructure/dto/SkillRequestDTO.java
@@ -1,8 +1,11 @@
 package com.nucleo42.infrastructure.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import jakarta.validation.constraints.NotNull;
 
 public record SkillRequestDTO(
         @NotNull(message = "The 'id' field is required")
+        @JsonProperty("skill_id")
         Long id) {
 }
--- infrastructure/src/main/java/com/nucleo42/infrastructure/dto/UpdateSkillRequestDTO.java
+++ infrastructure/src/main/java/com/nucleo42/infrastructure/dto/UpdateSkillRequestDTO.java
@@ -1,14 +1,18 @@
 package com.nucleo42.infrastructure.dto;
 
 import java.util.List;
+import java.util.UUID;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
 
 import jakarta.validation.Valid;
-import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 
 public record UpdateSkillRequestDTO(
-        @NotBlank(message = "The 'userId' field is required")
-        String userId,
+        @NotNull(message = "The 'user_id' field is required")
+        @JsonProperty("user_id")
+        UUID id,
         @NotEmpty(message = "The 'skills' field is required")
         List<@Valid SkillRequestDTO> skills) {
 }
