934f2741cc83c129d18010937f7d5146ca6b7995
Mikhael D'Amato <damato578@gmail.com>
2025-04-30 07:12:35 -0300

    refactor: clear CreateProjectRequestDTO

--- infrastructure/src/main/java/com/nucleo42/infrastruture/dto/CreateProjectRequestDTO.java
+++ infrastructure/src/main/java/com/nucleo42/infrastruture/dto/CreateProjectRequestDTO.java
@@ -1,6 +1,5 @@
 package com.nucleo42.infrastruture.dto;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
@@ -10,11 +9,14 @@ public record CreateProjectRequestDTO (
         @Size(min = 3, message = "O nome deve ter no mínimo 3 caracteres")
         @NotBlank(message = "O nome do projeto precisa ser informado")
         String name,
+
         @NotBlank(message = "A descrição do projeto precisa ser informado")
         String description,
+
         @NotNull(message = "As vagas do projeto precisam ser informadas")
         @Min(value = 0, message = "O total de vagas deve ser um número natural")
         Integer vacancies,
+
         @NotBlank(message = "O objetivo do projeto precisa ser informado")
         String goal
 ){
