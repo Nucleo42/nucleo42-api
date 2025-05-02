8248f9c64aef62a53514eb8a9ce66320eac3796b
Mikhael D'Amato <damato578@gmail.com>
2025-05-02 20:23:54 -0300

    feat: add FindProjectById bean in ProjectConfig

--- infrastructure/src/main/java/com/nucleo42/infrastructure/config/ProjectConfig.java
+++ infrastructure/src/main/java/com/nucleo42/infrastructure/config/ProjectConfig.java
@@ -1,15 +1,10 @@
 package com.nucleo42.infrastructure.config;
 
-import com.nucleo42.application.gateway.AddProjectGateway;
-import com.nucleo42.application.gateway.FindAllProjectsGateway;
-import com.nucleo42.application.gateway.RemoveProjectGateway;
-import com.nucleo42.application.gateway.UpdateProjectGateway;
-import com.nucleo42.application.usecase.AddProjectImpl;
-import com.nucleo42.application.usecase.FindAllProjectsImpl;
-import com.nucleo42.application.usecase.RemoveProjectImpl;
-import com.nucleo42.application.usecase.UpdateProjectImpl;
+import com.nucleo42.application.gateway.*;
+import com.nucleo42.application.usecase.*;
 import com.nucleo42.infrastructure.service.ProjectGateway;
 import com.nucleo42.usecase.AddProject;
+import com.nucleo42.usecase.FindProjectById;
 import com.nucleo42.usecase.findall.FindAllProjects;
 import com.nucleo42.usecase.RemoveProject;
 import com.nucleo42.usecase.UpdateProject;
@@ -33,6 +28,11 @@ public class ProjectConfig {
     @Bean
     public FindAllProjects findAllProjects(FindAllProjectsGateway findAllProjectsGateway) {
         return new FindAllProjectsImpl(findAllProjectsGateway);
+    }
+
+    @Bean
+    public FindProjectById findProjectById(FindProjectByIdGateway findProjectByIdGateway) {
+        return new FindProjectByIdImpl(findProjectByIdGateway);
     }
 
     @Bean
